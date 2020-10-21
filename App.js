1be9ce710e54a3eadc00710402f6d3d9fcea65fd
Sebarbossus <nicoalvareza22@gmail.com>
2020-10-21 20:20:14 +0200

    Added some padding

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>A minimalistic app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 40,
+    padding: 5
+  }
 });
