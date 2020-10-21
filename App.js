ddd1438d4c3ea9769bfb8349eb0d9fd55e59a8f6
Sebarbossus <nicoalvareza22@gmail.com>
2020-10-21 22:21:05 +0200

    Removed dependency

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import {base} from './styles/base';
 import {app} from './styles/app';
