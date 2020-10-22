925c446a332f15018be362f7ede4144a6c292a00
Sebarbossus <nicoalvareza22@gmail.com>
2020-10-22 15:58:42 +0200

    Added components

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import {app} from './styles/app';
 export default function App() {
   return (
     <View style={app.container}>
-      <Text style={app.text}>A minimalistic app</Text>
+      <Text style={app.text}>Not a minimalistic app</Text>
       <StatusBar style="auto" />
     </View>
   );
